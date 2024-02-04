097c30305074db37405a60a45dc41d882dd3928b
Jia Wang <wj2016@gmail.com>
2024-02-04 02:52:14 +0100

    defn compiler

--- config/config.exs
+++ config/config.exs
@@ -3,6 +3,7 @@ import Config
 config :test_cuda, :test_user, "Jia"
 
 config :nx, :default_backend, EXLA.Backend
+config :nx, :default_defn_options, compiler: EXLA
 
 # https://hexdocs.pm/exla/EXLA.html#module-clients
 config :exla, :default_client, :cuda
