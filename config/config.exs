559a6041d8d99dcef6a8df23721c635fb209609e
Jia Wang <wj2016@gmail.com>
2024-02-04 02:38:15 +0100

    cuda

--- config/config.exs
+++ config/config.exs
@@ -1,3 +1,8 @@
 import Config
 
 config :test_cuda, :test_user, "Jia"
+
+config :nx, :default_backend, EXLA.Backend
+
+# https://hexdocs.pm/exla/EXLA.html#module-clients
+config :exla, :default_client, :cuda
